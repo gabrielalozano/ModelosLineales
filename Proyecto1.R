2c40fb5f9a3d8dba6970738449a93c91002c65af
gabrielalozano <60881709+gabrielalozano@users.noreply.github.com>
2020-09-26 14:10:14 -0400

    Create Proyecto1.R

---
+++ Proyecto1.R
@@ -0,0 +1,76 @@
+#Exercise 4.2 generalized linear models
+
+timedeath = c(65,156,100,134,16,108,121,4,39,143,56,26,22,1,1,5,65)
+whitecell = c(3.36 ,2.88 ,3.63 ,3.41 , 3.78 , 4.02 , 4.00 , 4.23 , 3.73 , 3.85 , 3.97 , 4.51 , 4.54 , 5.00 , 5.00 , 4.72 , 5.00)
+
+leukemia = data.frame(timedeath, whitecell)
+
+library(ggplot2)
+#4.2 a
+
+#Plot
+ggplot(leukemia, aes(timedeath, whitecell))+ 
+  geom_point() +
+  ggtitle("Survival Time vs Initial White Blood Cell Count") +
+  xlab("Survival Time (Weeks)") + ylab("Initial White Blood Cell Count")+
+  geom_smooth(method = "lm")
+
+# as y decreses, x increases (y appears to decrease exponentially)  
+
+#4.2 b <  log, because log is a function that tranforms the equation to be linear
+# The link function is the function of y that gives you Xb on the right hand side.
+
+# 4.2 c ?
+
+# 4.2 d
+# log on both sides E[log(Y)] = b1 + b_2 X
+
+summary(lm(log(timedeath) ~ (whitecell), data=leukemia))
+
+# b1 = 11.0738 b2= -1.8829
+
+
+# 4.2 e
+
+#Predicted y-hats
+leukemia["Predicted"] = exp(11.0738 - 1.8829 *(leukemia[2]))
+
+# Linear models time of death with predicted
+leukpred = lm((timedeath) ~ Predicted, data=leukemia) 
+#Standarized Residuals
+leukpred.stand = rstandard(leukpred)
+#Plot
+plot(leukemia$timedeath, leukpred.stand, ylab="Standardized Residuals", xlab="Waiting Time", main="Leukemia") 
+abline(0, 0)                  # the horizon
+
+#Model fits the data well.
+
+
+#5.4 a ? 
+firstmodel = lm(log(timedeath) ~ (whitecell), data=leukemia)
+
+confint(firstmodel, level=0.90)
+
+
+
+WaldTest = function(L,thetahat,Vn,h=0) # H0: L theta = h
+  # Note Vn is the asymptotic covariance matrix, so it's the
+  # Consistent estimator divided by n. For true Wald tests
+  # based on numerical MLEs, just use the inverse of the Hessian.
+{
+  WaldTest = numeric(3)
+  names(WaldTest) = c("W","df","p-value")
+  r = dim(L)[1]
+  W = t(L%*%thetahat-h) %*% solve(L%*%Vn%*%t(L)) %*%
+    (L%*%thetahat-h)
+  W = as.numeric(W)
+  pval = 1-pchisq(W,r)
+  WaldTest[1] = W; WaldTest[2] = r; WaldTest[3] = pval
+  WaldTest
+} # End function WaldTest
+WaldTest()          
+
+
+
+
+        
